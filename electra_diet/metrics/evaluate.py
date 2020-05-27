9f9c1f508617645cb2af013188d0150a392ca413
seujung <digit82@gmail.com>
2020-05-27 11:42:48 +0900

    bug fixed

--- electra_diet/metrics/evaluate.py
+++ electra_diet/metrics/evaluate.py
@@ -108,7 +108,7 @@ def show_entity_report(dataset, pl_module, file_name=None, output_dir=None, cuda
 
         for i in range(entity_idx.shape[0]):
             decode_original = decoder.process(input_ids[i].cpu().numpy(), entity_idx[i].numpy())
-            decode_pred = decoder.process(input_ids[i].numpy(), entity_indices[i].numpy())
+            decode_pred = decoder.process(input_ids[i].cpu().numpy(), entity_indices[i].numpy())
 
             for origin in decode_original:
                 labels.add(origin['entity'])
