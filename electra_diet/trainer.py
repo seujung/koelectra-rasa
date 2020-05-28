e619aabea9d0a8401f3f0e06deea6fbbddbaf650
seujung <digit82@gmail.com>
2020-05-28 17:24:21 +0900

    add root path

--- electra_diet/trainer.py
+++ electra_diet/trainer.py
@@ -11,34 +11,6 @@ import torch
 from torch.utils.data import DataLoader
 
 from electra_diet.eval import PerfCallback
-# from pytorch_lightning.callbacks.base import Callback
-# from electra_diet.metrics import show_intent_report
-
-# class PerfCallback(Callback):
-#     def __init__(self, file_path=None, gpu_num=0, report_nm=None, output_dir=None):
-#         self.file_path = file_path
-#         if gpu_num > 0:
-#             self.cuda = True
-#         else:
-#             self.cuda = False
-#         self.report_nm = report_nm
-#         self.output_dir = output_dir
-
-#     def on_train_end(self, trainer, pl_module):
-#         print("train finished")
-#         if self.file_path is None:
-#             dataset = pl_module.val_dataset
-#         else:
-#             dataset = ElectraDataset(file_path=self.file_path, tokenizer=None)
-                
-#         if self.output_dir is None:
-#             path = 'lightning_logs/'
-#             folder_path = [f for f in glob.glob(path + "**/", recursive=False)]
-#             folder_path.sort()
-#             self.output_dir  = folder_path[-1]
-#         self.output_dir = os.path.join(self.output_dir, 'results')
-#         show_intent_report(dataset, pl_module, file_name=self.report_nm, output_dir=self.output_dir, cuda=self.cuda)
-
 
 def train(
     file_path,
@@ -60,7 +32,7 @@ def train(
     gpu_num = torch.cuda.device_count()
 
     trainer = Trainer(
-        default_root_dir=checkpoint_path, max_epochs=max_epochs, gpus=gpu_num, callbacks=[PerfCallback(gpu_num=gpu_num, report_nm=report_nm)]
+        default_root_dir=checkpoint_path, max_epochs=max_epochs, gpus=gpu_num, callbacks=[PerfCallback(gpu_num=gpu_num, report_nm=report_nm, root_path=checkpoint_path)]
     )
 
     model_args = {}
