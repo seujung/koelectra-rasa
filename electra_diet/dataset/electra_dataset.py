33aca7d58d01ca362252b7996a48c1e22712c963
seujung <digit82@gmail.com>
2020-06-02 09:38:59 +0900

    add exception

--- electra_diet/dataset/electra_dataset.py
+++ electra_diet/dataset/electra_dataset.py
@@ -142,30 +142,31 @@ class ElectraDataset(torch.utils.data.Dataset):
                     text = text.replace("[", "").replace("]", "")
 
                     each_data_dict = {}
-                    each_data_dict["text"] = text.strip()
-                    each_data_dict["intent"] = current_intent_focus
-                    each_data_dict["intent_idx"] = self.intent_dict[
-                        current_intent_focus
-                    ]
-                    each_data_dict["entities"] = []
-
-                    for value, type_str in zip(entity_value_list, entity_type_list):
-                        try:
-                            for entity in re.finditer(value, text):
-                                each_data_dict["entities"].append(
-                                    {
-                                        "start": entity.start(),
-                                        "end": entity.end(),
-                                        "entity": type_str,
-                                        # "entity_idx": self.entity_dict[type_str],
-                                    }
-                                )
-                        except Exception as ex:
-                            print(f"error occured : {ex}")
-                            print(f"value: {value}")
-                            print(f"text: {text}")
-
-                    self.dataset.append(each_data_dict)
+                    if len(text.strip() > 1):
+                        each_data_dict["text"] = text.strip()
+                        each_data_dict["intent"] = current_intent_focus
+                        each_data_dict["intent_idx"] = self.intent_dict[
+                            current_intent_focus
+                        ]
+                        each_data_dict["entities"] = []
+    
+                        for value, type_str in zip(entity_value_list, entity_type_list):
+                            try:
+                                for entity in re.finditer(value, text):
+                                    each_data_dict["entities"].append(
+                                        {
+                                            "start": entity.start(),
+                                            "end": entity.end(),
+                                            "entity": type_str,
+                                            # "entity_idx": self.entity_dict[type_str],
+                                        }
+                                    )
+                            except Exception as ex:
+                                print(f"error occured : {ex}")
+                                print(f"value: {value}")
+                                print(f"text: {text}")
+    
+                        self.dataset.append(each_data_dict)
 
         print(f"Intents: {self.intent_dict}")
         if self.tag_type == 'bio':
