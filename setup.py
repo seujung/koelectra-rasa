03909c791e4f0696c6e88ed69b6d1d3aa118b050
seujung <digit82@gmail.com>
2020-07-06 12:47:58 +0900

    add install required

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version="0.2",
     description="Koelecta based intent / entity model",
     author="seujung",
-    install_requires=[],
+    install_requires=required,
     packages=find_packages(exclude=["docs", "tests", "tmp", "data"]),
     python_requires=">=3",
     zip_safe=False,
