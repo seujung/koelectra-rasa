db3f1283a3b7010057d1e3458f40e200a181bd6f
seujung <digit82@gmail.com>
2020-08-17 22:50:25 +0900

    upate setup

--- electra_diet/__init__.py
+++ electra_diet/__init__.py
@@ -3,3 +3,4 @@ from .pl_model import KoELECTRAClassifier
 from .inferencer import Inferencer
 from .tokenizer import tokenize, get_tokenizer, delete_josa
 from .postprocessor import ner_decoder
+from . import chatspace
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="Koelecta based intent / entity model",
     author="seujung",
     install_requires=required,
-    packages=find_packages(exclude=["docs", "tests", "tmp", "data"]),
+    packages=find_packages(exclude=["docs", "tests", "tmp"]),
     python_requires=">=3",
     zip_safe=False,
     include_package_data=True,
